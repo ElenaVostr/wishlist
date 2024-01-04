e44b2d320db4ee5d0795886abdbb65f4dbaf782b
ElenaKova <elenakova91@yandex.ru>
2024-01-05 01:07:00 +0300

    Бизнес логика фичи желания
    Реализована модель желания и юзкейсы на создание, редактирование и удаление желания

---
+++ lib/domain/enums/wish_status.dart
@@ -0,0 +1,1 @@
+enum WishStatus { undone, done, archived, reserved }
---
+++ lib/domain/repositories/wish_repository.dart
@@ -0,0 +1,10 @@
+import 'package:wishlist/domain/models/wish.dart';
+
+abstract class WishRepository {
+  Future<void> createWish(Wish wish);
+
+  Future<void> replaceWish(Wish wish);
+
+  Future<void> deleteWish(int uid);
+
+}
