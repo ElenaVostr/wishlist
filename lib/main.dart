d215bbcdc175a0ed61050f4996790196f1a81611
ElenaKova <elenakova91@yandex.ru>
2024-01-07 17:31:04 +0300

    внесла изменения в ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:wishlist/data/firebase_service.dart';
-import 'package:wishlist/data/repositories/wish_repository_impl.dart';
+import 'package:wishlist/di/di.dart';
 import 'package:wishlist/domain/models/wish.dart';
+import 'package:wishlist/domain/usecases/create_wish_usecase.dart';
+import 'package:wishlist/domain/usecases/get_wish_list_stream_usecase.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  FirebaseService firebaseService = FirebaseService();
-  await firebaseService.init();
+  await DI.registerDependencies();
   runApp(const MyApp());
 }
 
@@ -23,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Wish List'),
     );
   }
 }
@@ -38,25 +37,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late CollectionReference<Wish> _wishesRef;
-  late FirebaseService _firebaseService;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _firebaseService = FirebaseService();
-
-    _wishesRef = _firebaseService.firestore
-        .collection('wishes')
-        .withConverter<Wish>(
-            fromFirestore: (snapshot, _) => Wish.fromJson(snapshot.data()!, snapshot.id),
-            toFirestore: (wish, _) => wish.toJson());
-  }
 
   Future<void> _createWish() async {
-    WishRepositoryImpl wishRepositoryImpl = WishRepositoryImpl(firebaseService: _firebaseService);
-    wishRepositoryImpl.createWish(const Wish(name: 'новое желание'));
+    final useCase = DI.getit.get<CreateWishUseCase>();
+    useCase.run(const Wish(name: 'новое желание'));
   }
 
   @override
@@ -72,17 +56,30 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Expanded(
                 child: StreamBuilder<List<Wish>>(
-              stream: _wishesRef
-                  .snapshots()
-                  .map((event) => event.docs.map((e) => e.data()).toList()),
+              stream: DI.getit.get<GetWishListStreamUseCase>().run(),
               builder: (context, snapshot) {
                 return ListView(
                     children: snapshot.hasData
                         ? snapshot.data!
-                        .map((e) => ListTile(
-                        leading: Text(e.name),
-                        trailing: Text(e.description)))
-                        .toList()
+                            .map((e) => ListTile(
+                                  leading: e.images.isNotEmpty
+                                      ? Image.network(e.images.first,
+                                          width: 50,
+                                          height: 50,
+                                          fit: BoxFit.fill)
+                                      : Image.asset(
+                                          'assets/images/image_icon.png',
+                                          width: 50,
+                                          height: 50,
+                                          fit: BoxFit.fill),
+                                  title: Text(e.name),
+                                  onTap: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) {
+                                    return WishPage(wish: e);
+                                  })),
+                                ))
+                            .toList()
                         : []);
               },
             ))
@@ -94,6 +91,57 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), //// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class WishPage extends StatefulWidget {
+  const WishPage({super.key, required this.wish});
+
+  final Wish wish;
+
+  @override
+  State<WishPage> createState() => _WishPageState();
+}
+
+class _WishPageState extends State<WishPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: (widget.wish.images.isNotEmpty ? NetworkImage(widget.wish.images.first) : const AssetImage('assets/images/image_icon.png')) as ImageProvider,
+                )
+              ),
+            ),
+            // widget.wish.images.isNotEmpty
+            //     ? Image.network(widget.wish.images.first, height: 200, width: double.maxFinite, fit: BoxFit.fill,)
+            //     : Image.asset('assets/images/image_icon.png', height: 200, width: double.maxFinite, fit: BoxFit.fill,)
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _createWish,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), //// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
